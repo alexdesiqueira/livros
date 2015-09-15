9732e89c11b88d6241493f86030dee07bb970c8b
alexandrejaguar <siqueiraaf@gmail.com>
2015-09-15 13:55:39 -0300

    Códigos. Cap 7

---
+++ 2015/compcieoctave/cap7/sec7.1.m
@@ -0,0 +1,61 @@
+%{
+SEC7.1.M
+
+Material de apoio do livro "Computação científica com GNU Octave".
+Exemplos da seção 7.1, capítulo 7, "O Octave-Forge: mais poder para 
+seu Octave".
+
+* Autor: Alexandre 'Jaguar' Fioravante de Siqueira
+* Editora: Casa do Código
+* Contato: http://www.programandociencia.com/sobre/
+           http://www.casadocodigo.com.br/
+* Fórum de discussão: http://groups.google.com/d/forum/compcieoctave
+* Material de apoio: http://goo.gl/VnnAnF
+
+* Para citar esse material, por favor utilize a referência abaixo:
+SIQUEIRA, A.F. Computação científica com GNU Octave. São Paulo: 
+Casa do Código, 2015. xxx p.
+
+Este programa é um software livre; você pode redistribuí-lo e/ou 
+modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+Licença, ou qualquer versão posterior.
+
+Este programa é distribuído na esperança de que possa ser útil, 
+mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+Licença Pública Geral GNU para maiores detalhes.
+
+Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+com este programa. Se não, veja <http://www.gnu.org/licenses/>.
+%}
+
+% #1
+pkg install -forge signal
+
+% Nota 1
+sudo apt-get install liboctave-dev
+
+% #2
+pkg install -forge bim
+
+% #3
+pkg install -forge bim fpl msh
+
+% #4
+pkg install -forge bim fpl msh splines
+
+% #5
+pkg load bim
+
+% #6
+pkg load all
+
+% #7
+pkg unload nomedopacote
+
+% #8
+pkg unload all
+
+% #9
+pkg uninstall signal
